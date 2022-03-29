1723e6d6c8100eece319baaae71d6eede535892e
Dave Welsh <dave@qccareerschool.com>
2022-03-29 14:50:14 -0400

    Convert MyApp to a class component

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -23,8 +23,18 @@ if (!TrackJS.isInstalled()) {
   });
 }
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+class MyApp extends App {
+
+  componentDidCatch(error, errorInfo) {
+    TrackJS.track(error);
+    this.setState({ error });
+    // Anything else you want to do with the error.
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />
+  }
 }
 
 export default MyApp;
